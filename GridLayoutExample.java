ccf92d9351b133e91788edbba4e167ec35d679d0
CodeBlooded <codeblooded589@gmail.com>
2026-03-28 16:28:32 +0300

    Modified for testing
    Modified for testing

--- GridLayoutExample.java
+++ GridLayoutExample.java
@@ -6,28 +6,42 @@ import java.awt.*;
 public class GridLayoutExample {
 
     public GridLayoutExample() {
+        this.CreateFrame();
+    }
+
+    public JFrame CreateFrame() {
         JFrame frame = new JFrame();
         frame.setTitle("GridLayout Demo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-        JPanel mainContainer = new JPanel(new GridLayout(1, 3));
-
-        JPanel panel1 = new JPanel();
-        panel1.setBackground(Color.RED);
-
-        JPanel panel2 = new JPanel();
-        panel2.setBackground(Color.GREEN);
-
-        JPanel panel3 = new JPanel();
-        panel3.setBackground(Color.BLUE);
-
-        mainContainer.add(panel1);
-        mainContainer.add(panel2);
-        mainContainer.add(panel3);
-        
-        frame.add(mainContainer);
+        frame.add(CreateMainPanel());
         frame.setVisible(true);
+        return frame;
+    }
 
+    public JPanel CreateMainPanel() {
+        JPanel mainContainer = new JPanel(new GridLayout(1, 3));
+        mainContainer.add(CreatePanelOne());
+        mainContainer.add(CreatePanelTwo());
+        mainContainer.add(CreatePanelThree());
+        return mainContainer;
+    }
+
+    public JPanel CreatePanelOne() {
+        JPanel panelOne = new JPanel();
+        panelOne.setBackground(Color.ORANGE);
+        return panelOne;
+    }
+
+    public JPanel CreatePanelTwo() {
+        JPanel panelTwo = new JPanel();
+        panelTwo.setBackground(Color.GRAY);
+        return panelTwo;
+    }
+
+    public JPanel CreatePanelThree() {
+        JPanel panelThree = new JPanel();
+        panelThree.setBackground(Color.MAGENTA);
+        return panelThree;
     }
 }
